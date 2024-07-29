87a987c719238c939cf22bf8c3cde3ea78740ff8
ludimilla23 <147458759+ludimilla23@users.noreply.github.com>
2024-07-29 19:55:17 -0300

    Convertendo Km em metros, cm e milímetros.
    Uma conversão bem básica.

---
+++ ConversaoKm.py
@@ -0,0 +1,5 @@
+KM = float(input("Digite os km para conversão: "))
+metros = KM * 1000  # 1 km = 1000 metros
+cm = KM * 100000  # 1 km = 100000 centímetros
+mm = KM * 1000000  # 1 km = 1000000 milímetros
+print(f"A conversão é igual a {metros} metros, {cm} centímetros e {mm} milímetros.")
